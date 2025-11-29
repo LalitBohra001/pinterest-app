d090042c11998ba467912b061b935e8932eb6cba
LalitBohra001 <lalit.d.bohra@gmail.com>
2025-11-29 17:28:37 +0530

    commit

---
+++ pinterest-backend/src/main/java/Minimal/Pinterest_like/Spring/Boot/backend/Azure/Blob/integration/PinterestBackendApplication.java
@@ -0,0 +1,13 @@
+package Minimal.Pinterest_like.Spring.Boot.backend.Azure.Blob.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinterestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinterestBackendApplication.class, args);
+	}
+
+}
---
+++ pinterest-backend/src/test/java/Minimal/Pinterest_like/Spring/Boot/backend/Azure/Blob/integration/PinterestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Minimal.Pinterest_like.Spring.Boot.backend.Azure.Blob.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinterestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
